c1ce45dfc98cdd1b88bee0b3f604a18d1daee2b5
Brian <briangrji28@gmail.com>
2023-06-29 13:58:01 -0600

    mail format to send the token

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -32,7 +32,105 @@ async function sendToken(email) {
     from: emailTransporter,
     to: email,
     subject: "Token de autenticación de doble factor",
-    text: "Aquí está tu token de autenticación: " + token,
+    html: `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+    
+        <style>
+          @import url("https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&display=swap");
+          @import url("https://fonts.googleapis.com/css2?family=Geologica:wght@100;200;300;400;500;600;700;800;900&display=swap");
+    
+          * {
+            margin: 0;
+            padding: 0;
+          }
+    
+          .contenedor-imagen{
+            padding: 50px;
+            padding-bottom: 0px;
+            align-items: center;
+            text-align: center;
+          }
+    
+          img{
+            width: auto;
+            height: 100px;
+          }
+    
+          .contenedor-texto {
+            padding: 50px 50px 50px 50px;
+            font-family: "Heebo", sans-serif;
+            font-weight: 300;
+          }
+    
+          .contenedor-token{
+            display: flex;
+            flex-direction: row;
+            font-family: "Heebo", sans-serif;
+            font-size: x-large;
+          }
+    
+          .text-token{
+            margin-left: 2px;
+            font-weight: bold;
+          }
+    
+          footer{
+            background-color: #202020;
+            padding: 50px;
+            align-items: center;
+            text-align: center;
+          }
+    
+          .footer-text{
+            color: white;
+            font-family: "Heebo", sans-serif;
+            font-weight: 500;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="contenedor-imagen">
+          <img src="https://tiusr3pl.cuc-carrera-ti.ac.cr/clase/brian/progra05/logo.png" alt="">
+        </div>
+        <div class="contenedor-texto">
+          <p>
+            Kind regards. We are pleased to inform you that we have received a
+            request to reset your account password. To ensure the protection of your
+            personal data, we have generated a unique security token that you will
+            need to use to complete the reset process.
+          </p>
+          <br />
+          <p>
+            Below you will find the token needed to proceed with your password
+            reset:
+          </p>
+          <br />
+          <div class="contenedor-token">
+            <p>Security Token: </p>
+            <p class="text-token"> ${token}</p>
+          </div>
+          
+          <br />
+          <p>
+            If you have NOT requested a password reset, we strongly urge you to
+            <strong>delete this email immediately</strong> and take the necessary
+            steps to protect your account. If you have any concerns or suspect that
+            your account may be at risk, we encourage you to contact our support
+            team as soon as possible.
+          </p>
+        </div>
+        <footer>
+          <div>
+            <p class="footer-text">Project Development Team</p>
+          </div>
+        </footer>
+      </body>
+    </html>
+    
+    `,
   });
 
   // El secreto se usará adelante para aplicar la validación
@@ -54,11 +152,12 @@ function validateToken(userEnteredToken, tempSecret) {
 }
 
 // Funcion para saber si el token a expirado
-function validateTimeToken(tokenGeneration){
+function validateTimeToken(tokenGeneration) {
   const currentDate = new Date();
   const tokenGenerationDate = new Date(tokenGeneration);
 
-  const diferenciaMilisegundos = currentDate.getTime() - tokenGenerationDate.getTime();
+  const diferenciaMilisegundos =
+    currentDate.getTime() - tokenGenerationDate.getTime();
   const diferenciaMinutos = Math.floor(diferenciaMilisegundos / (1000 * 60));
 
   return diferenciaMinutos < 5;
@@ -101,8 +200,8 @@ const registerController = {
 
       const tokenData = {
         tokenGenerationDate: fechaGeneracionToken,
-        tempSecret: secret
-      }
+        tempSecret: secret,
+      };
 
       //Almacenar en variable de session datos del registro de la cuenta
       req.session.userData = userData;
@@ -183,9 +282,9 @@ const registerController = {
       const isValidToken = validateToken(tokenEntered, tokenData.tempSecret);
 
       if (!isValidToken) {
-        const tokenExpired = validateTimeToken(tokenData.tokenGenerationDate)
-        
-        if(!tokenExpired){
+        const tokenExpired = validateTimeToken(tokenData.tokenGenerationDate);
+
+        if (!tokenExpired) {
           return res.render("login", {
             errorMessage:
               "The token has expired, you must start the process again!",
@@ -193,8 +292,7 @@ const registerController = {
         }
 
         return res.render("registrationPersonalData", {
-          errorMessage:
-            "The token entered is invalid!",
+          errorMessage: "The token entered is invalid!",
         });
       }
 
