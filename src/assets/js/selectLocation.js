9841a9f8ee82752dc708f07eaf30ca9b7f150069
Brian <briangrji28@gmail.com>
2023-07-14 20:47:25 -0600

    AJAX request script to upload location dataAJAX request script to upload location data

---
+++ src/assets/js/selectLocation.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const provinciaDropdown = document.getElementById("provincia");
+  const cantonDropdown = document.getElementById("canton");
+  const distritoDropdown = document.getElementById("distrito");
+
+  provinciaDropdown.addEventListener("change", function () {
+    const provinciaSeleccionada = provinciaDropdown.value;
+
+    // Reiniciar los dropdowns de cantones y distritos
+    cantonDropdown.innerHTML = '<option value="">Seleccione un cantón</option>';
+    distritoDropdown.innerHTML =
+      '<option value="">Seleccione un distrito</option>';
+
+    if (provinciaSeleccionada) {
+      // Realizar una solicitud AJAX para obtener los cantones de la provincia seleccionada
+      const xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          const cantones = JSON.parse(this.responseText);
+          cantones.forEach(function (canton) {
+            const option = document.createElement("option");
+            option.value = canton.nombre;
+            option.textContent = canton.nombre;
+            cantonDropdown.appendChild(option);
+          });
+        }
+      };
+      xhttp.open(
+        "GET",
+        "/get-cantons/" + encodeURIComponent(provinciaSeleccionada),
+        true
+      );
+      xhttp.send();
+    }
+  });
+
+  cantonDropdown.addEventListener("change", function () {
+    const cantonSeleccionado = cantonDropdown.value;
+
+    // Reiniciar el dropdown de distritos
+    distritoDropdown.innerHTML =
+      '<option value="">Seleccione un distrito</option>';
+
+    if (cantonSeleccionado) {
+      // Realizar una solicitud AJAX para obtener los distritos del cantón seleccionado
+      const xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          const distritos = JSON.parse(this.responseText);
+          distritos.forEach(function (distrito) {
+            const option = document.createElement("option");
+            option.value = distrito;
+            option.textContent = distrito;
+            distritoDropdown.appendChild(option);
+          });
+        }
+      };
+      xhttp.open(
+        "GET",
+        "/get-districts/" + encodeURIComponent(cantonSeleccionado),
+        true
+      );
+      xhttp.send();
+    }
+  });
+});
