e678f191c4d44972cd960ad7de9ed128eefb11e5
Brian Granados <bragrji@gmail.com>
2023-08-17 14:10:18 -0600

    Mejoras del perfil del usuario

--- src/assets/js/sliderButtons.js
+++ src/assets/js/sliderButtons.js
@@ -3,40 +3,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Se obtienen todos los contenedores de sliders
   const contenedorSliders = document.querySelectorAll(".contenedor-slider");
+  
 
   contenedorSliders.forEach((contenedorSlider) => {
+    const slider = contenedorSlider.querySelector(".slider");
+    let intervalId;
+
+    function startAutoScroll() {
+      intervalId = setInterval(() => {
+        scrollSlider(slider, "next");
+      }, 3000);
+    }
+
+    function stopAutoScroll() {
+      clearInterval(intervalId);
+    }
+
+    startAutoScroll();
+
     const botonPrev = contenedorSlider.querySelector(".slider-nav-boton-prev");
     const botonNext = contenedorSlider.querySelector(".slider-nav-boton-next");
-    const slider = contenedorSlider.querySelector(".slider");
-    
-    // Se obtienen los botones y el propio slider para cada contenedor de slider
-    botonPrev.addEventListener("click", () => scrollSlider(slider, "prev"));
-    botonNext.addEventListener("click", () => scrollSlider(slider, "next"));
+
+    botonPrev.addEventListener("click", () => {
+      stopAutoScroll();
+      scrollSlider(slider, "prev");
+      startAutoScroll();
+    });
+
+    botonNext.addEventListener("click", () => {
+      stopAutoScroll();
+      scrollSlider(slider, "next");
+      startAutoScroll();
+    });
   });
 
   function scrollSlider(slider, direccionDeslizamiento) {
-
-    // Obtener el ancho del slider
     const containerWidth = slider.offsetWidth;
-
-     // Obtener el ancho del elemento individual
-    const slideWidth = getComputedStyle(
-      slider.querySelector(".slider-elemento")
-    ).width;
-
-     // Calcular el numero de elementos visibles basado en el ancho del slider
+    const slideWidth = getComputedStyle(slider.querySelector(".slider-elemento")).width;
     const slideCount = Math.floor(containerWidth / parseInt(slideWidth));
-
-    // Calcular la cantidad de desplazamiento(este valor puede ser negativo)
     const cantidadDesplazamiento =
       direccionDeslizamiento === "prev"
         ? -slideCount * parseInt(slideWidth)
         : slideCount * parseInt(slideWidth);
 
-    // scrollBy  es una funcion para desplazar el carousel
-    slider.scrollBy({
-      left: cantidadDesplazamiento,
-      behavior: "smooth",
-    });
+    // Comprueba si se ha llegado al final y ajusta el desplazamiento
+    if (direccionDeslizamiento === "next" && slider.scrollLeft + containerWidth >= slider.scrollWidth) {
+      slider.scrollLeft = 0;
+    } else if (direccionDeslizamiento === "prev" && slider.scrollLeft === 0) {
+      slider.scrollLeft = slider.scrollWidth - containerWidth;
+    } else {
+      slider.scrollBy({
+        left: cantidadDesplazamiento,
+        behavior: "smooth",
+      });
+    }
   }
 });
